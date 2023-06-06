4ef7c1f16fbe332f8ae993612d0c2337faf94bdf
Wahyu Wardana <wahyu.wardana100@gmail.com>
2023-06-06 16:01:30 +0700

    add autentication and add database for disesase detailsnew deployment methode using cloud run to deploy, add new endpoint for DELETE, updste result on POST, add image preview link, add POST to google storage

--- Cloud Computing/disease_details/routes.py
+++ Cloud Computing/disease_details/routes.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, jsonify
-from disease_details.details import get_disease_details
-
-disease_details_bp = Blueprint('disease_details', __name__)
+from . import get_disease_details
+from . import disease_details_bp
 
 @disease_details_bp.route('/details/<disease_name>', methods=['GET'])
 def details(disease_name):
     disease_details, handling_method = get_disease_details(disease_name)
 
     response = {
-        'predicted_class': disease_name,
+        'disease_name': disease_name,
         'disease_details': disease_details,
         'handling_method': handling_method
     }
