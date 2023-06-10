6fce0b6cc8b8e43991a120acf5a4d56b047a399a
TrernakKu <134289835+TernakKu@users.noreply.github.com>
2023-06-10 14:55:06 +0700

    Update connection.py

--- Cloud Computing/connection.py
+++ Cloud Computing/connection.py
@@ -3,9 +3,9 @@ import pymysql
 
 def create_connection():
     connection = pymysql.connect(
-        host="34.101.184.157",
-        user="root",
-        password="root",
+        host="057",
+        user="",
+        password="",
         db="ternakku",
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor,
